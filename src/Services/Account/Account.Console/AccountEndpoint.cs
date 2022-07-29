11df288171e9af353fcce38d470caa98e98bf48c
Saiful Islam <saifulprogrammer@gmail.com>
2022-07-29 18:36:06 +0000

    added console app

---
+++ src/Services/Account/Account.Console/AccountEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Account.ConsoleApp
+{
+    public class AccountEndpoint
+    {
+        private readonly IMediator _mediator;
+
+        public AccountEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
---
+++ src/Services/Account/Account.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using Account.Application.Features.AccountActivity.Commands.Withdraw;
+using Account.Application.Features.AccountActivity.Queries.GetStatement;
+using Account.Application.Model;
+using Account.Domain.Entities;
+using System.Reflection;
+using Account.Application;
+using Account.Service;
+using Account.Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace Account.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //var serviceProvider = new ServiceCollection()
+            //    .AddApplicationServices()
+            var services = new ServiceCollection();
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+            ;
+            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddApplicationServices();
+            services.AddServices();
+            services.AddInfrastructureServices(Configuration);
+
+            Initialize();
+
+        }
+        static void Initialize()
+        {
+            Console.WriteLine("What");
+            //AutoMapper.Mapper.CreateMap<DepositCommand, Amount>().ReverseMap();
+            //CreateMap<WithdrawCommand, Amount>().ReverseMap();
+            //CreateMap<StatementVm, AccountActivity>().ReverseMap();
+        }
+
+
+    }
+}
