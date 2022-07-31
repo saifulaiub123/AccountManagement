17697777eeef5b041f57ae2533035e885506915d
Saiful Islam <saifulprogrammer@gmail.com>
2022-07-31 11:30:51 +0600

    remove unnecessary using

--- src/Services/Account/Account.Api/Extentions/HostExtention.cs
+++ src/Services/Account/Account.Api/Extentions/HostExtention.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Account.Infrastructure.Persistence;
 
 namespace Account.Api.Extentions
 {
